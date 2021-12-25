dbadb690204d137bdf86d7936354485e0375d756
Harish <harishpenta918@gmail.com>
2021-12-25 12:32:50 +0530

    Initial Commit.

--- app/src/main/java/com/demo/tracker/adapters/RunAdapter.kt
+++
@@ -1,9 +0,0 @@
-package com.demo.tracker.adapters
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class RunAdapter : RecyclerView.Adapter<RunAdapter.RunViewHolder>() {
-    inner class RunViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
-
-}
--- app/src/main/java/com/demo/tracker/ui/fragments/SettingsFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.demo.tracker.ui.fragments
-
-import androidx.fragment.app.Fragment
-import com.demo.tracker.R
-
-class SettingsFragment : Fragment(R.layout.fragment_settings) {
-}
